86672660e62dfc4a120230fd9e4ac2227337c46b
Maksim Kozhin <a.zurnaaaa@gmail.com>
2024-11-17 15:11:59 +0300

    make model OrderItem

--- autosales/models.py
+++ autosales/models.py
@@ -251,7 +251,7 @@ class Order(models.Model):
         User,
         verbose_name=_("User"),
         related_name="orders",
-        blank=True,
+        blank=False,
         on_delete=models.CASCADE,
     )
     date_time = models.DateTimeField(auto_now_add=True)
@@ -269,9 +269,36 @@ class Order(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Заказ'
+        verbose_name = "Заказ"
         verbose_name_plural = "Список заказ"
-        ordering = ('-date_time',)
+        ordering = ("-date_time",)
 
     def __str__(self) -> str:
         return f"{self.user}: {self.date_time}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        verbose_name=_("Order"),
+        related_name="ordered_items",
+        blank=False,
+        on_delete=models.CASCADE,
+    )
+    product_info = models.ForeignKey(
+        ProductInfo,
+        verbose_name=_("Product`s information"),
+        related_name="ordered_items",
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    quantity = models.PositiveIntegerField(verbose_name=_("Quantity"))
+
+    class Meta:
+        verbose_name = "Заказанная позиция"
+        verbose_name_plural = "Список заказанных позиций"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["order_id", "product_info"], name="unique_order_item"
+            ),
+        ]
