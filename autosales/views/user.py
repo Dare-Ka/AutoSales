d5c914ba1d3063c0f06d9c3ae4fd09a8a172c3ac
Maksim Kozhin <a.zurnaaaa@gmail.com>
2024-12-08 16:46:17 +0300

    add class api view for loging into account

--- autosales/views/user.py
+++ autosales/views/user.py
@@ -1,9 +1,9 @@
-from os import error
-
+from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import validate_password
-from django.http import JsonResponse
 from rest_framework import generics, status
+from rest_framework.authtoken.models import Token
 from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from autosales.models import User
@@ -33,9 +33,12 @@ class ConfirmAccountView(generics.CreateAPIView):
 
 
 class AccountDetails(generics.ListAPIView, generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
     def list(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return JsonResponse(
+            return Response(
                 {"Status": False, "Error": "Log in required"},
                 status=status.HTTP_403_FORBIDDEN,
             )
@@ -44,7 +47,7 @@ class AccountDetails(generics.ListAPIView, generics.CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return JsonResponse(
+            return Response(
                 {"Status": False, "Error": "Log in required"},
                 status=status.HTTP_403_FORBIDDEN,
             )
@@ -52,16 +55,40 @@ class AccountDetails(generics.ListAPIView, generics.CreateAPIView):
             try:
                 validate_password(request.data["password"])
             except ValidationError as error:
-                return JsonResponse(
-                    {"Status": False, "Errors": {"password": str(error)}}
-                )
+                return Response({"Status": False, "Errors": {"password": str(error)}})
             else:
                 request.user.set_password(request.data["password"])
 
         user_serializer = UserSerializer(request.user, data=request.data, partial=True)
         if user_serializer.is_valid():
             user_serializer.save()
-            return JsonResponse({'Status': True})
+            return Response({"Status": True})
         else:
-            return JsonResponse({'Status': False, 'Errors': user_serializer.errors})
+            return Response({"Status": False, "Errors": user_serializer.errors})
 
+
+class LoginAccount(generics.CreateAPIView):
+    serializer_class = ConfirmEmailTokenSerializer
+
+    def create(self, request, *args, **kwargs):
+        if {"email", "password"}.issubset(request.data):
+            user = authenticate(
+                request,
+                username=request.data["email"],
+                password=request.data["password"],
+            )
+            if user is not None and user.is_active:
+                token, created = Token.objects.get_or_create(user=user)
+                return Response(
+                    {"Status": True, "Token": token.key}, status=status.HTTP_200_OK
+                )
+
+            return Response(
+                {"Status": False, "Errors": "Не удалось авторизовать"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        return Response(
+            {"Status": False, "Errors": "Не указаны все необходимые аргументы"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
