294a1129fc1d336c3a85b363ee1b475a8e04f3d2
Maksim Kozhin <a.zurnaaaa@gmail.com>
2024-12-14 10:14:53 +0300

    add class api view for user contacts

--- autosales/views/user.py
+++ autosales/views/user.py
@@ -1,14 +1,15 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import validate_password
-from rest_framework import generics, status
+from django.db.models import Q
+from rest_framework import generics, status, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from autosales.models import User
+from autosales.models import User, Contact
 from autosales.serializers.auth_token import ConfirmEmailTokenSerializer
-from autosales.serializers.user import UserSerializer
+from autosales.serializers.user import UserSerializer, ContactSerializer
 
 
 class RegisterAccountView(generics.CreateAPIView):
@@ -62,7 +63,7 @@ class AccountDetails(generics.ListAPIView, generics.CreateAPIView):
         user_serializer = UserSerializer(request.user, data=request.data, partial=True)
         if user_serializer.is_valid():
             user_serializer.save()
-            return Response({"Status": True})
+            return Response({"Status": True}, status=status.HTTP_200_OK)
         else:
             return Response({"Status": False, "Errors": user_serializer.errors})
 
@@ -91,4 +92,95 @@ class LoginAccount(generics.CreateAPIView):
         return Response(
             {"Status": False, "Errors": "Не указаны все необходимые аргументы"},
             status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+class ContactView(viewsets.ModelViewSet):
+    def list(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response(
+                {"Status": False, "Error": "Log in required"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        contact = Contact.objects.filter(user_id=request.user.id)
+        serializer = ContactSerializer(contact, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response(
+                {"Status": False, "Error": "Log in required"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        if {"city", "street", "phone"}.issubset(request.data):
+            request.data._mutable = True
+            request.data.update({"user": request.user.id})
+            serializer = ContactSerializer(data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"Status": True}, status=status.HTTP_200_OK)
+            else:
+                return Response({"Status": False, "Errors": serializer.errors})
+
+        return Response(
+            {"Status": False, "Errors": "Не указаны все необходимые аргументы"},
+            status=status.HTTP_403_FORBIDDEN,
+        )
+
+    def update(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response(
+                {"Status": False, "Error": "Log in required"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        if "id" in request.data:
+            if request.data["id"].isdigit():
+                contact = Contact.objects.filter(
+                    id=request.data["id"], user_id=request.user.id
+                ).first()
+                print(contact)
+                if contact:
+                    serializer = ContactSerializer(
+                        contact, data=request.data, partial=True
+                    )
+                    if serializer.is_valid():
+                        serializer.save()
+                        return Response({"Status": True}, status=status.HTTP_200_OK)
+                    else:
+                        return Response({"Status": False, "Errors": serializer.errors})
+
+        return Response(
+            {"Status": False, "Errors": "Не указаны все необходимые аргументы"},
+            status=status.HTTP_403_FORBIDDEN,
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response(
+                {"Status": False, "Error": "Log in required"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        items_sting = request.data.get("items")
+        if items_sting:
+            items_list = items_sting.split(",")
+            query = Q()
+            objects_deleted = False
+            for contact_id in items_list:
+                if contact_id.isdigit():
+                    query = query | Q(user_id=request.user.id, id=contact_id)
+                    objects_deleted = True
+
+            if objects_deleted:
+                deleted_count = Contact.objects.filter(query).delete()[0]
+                return Response(
+                    {"Status": True, "Удалено объектов": deleted_count},
+                    status=status.HTTP_200_OK,
+                )
+        return Response(
+            {"Status": False, "Errors": "Не указаны все необходимые аргументы"},
+            status=status.HTTP_403_FORBIDDEN,
         )
